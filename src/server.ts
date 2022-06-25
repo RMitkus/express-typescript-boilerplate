342bfe8a437648dc881980821cc8cc2d169001ac
Rytis Mitkus <77438479+RytisMitkus@users.noreply.github.com>
2022-06-25 22:57:37 +0300

    resolved

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,16 @@
 import { config } from 'dotenv';
 import express, { Application, NextFunction, Request, Response } from 'express';
 
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-// use `prisma` in your application to read and write data in your DB
 config();
 
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'Alice@al.com',
-      password: '123456'
-    }
-  });
-  res.send('Express server with TypeScript');
+    res.send('Express server with TypeScript');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
