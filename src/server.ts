af98279a51c98b60a85e06ad333f505e56f74f61
Rytis Mitkus <77438479+RytisMitkus@users.noreply.github.com>
2022-06-25 22:57:55 +0300

    resolved

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,11 @@ config();
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Express server with TypeScript');
+  res.send('Express server with TypeScript');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
