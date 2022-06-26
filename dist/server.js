b47a684879a3a2d29eb7398aaaa4338272df988b
Rytis Mitkus <77438479+RytisMitkus@users.noreply.github.com>
2022-06-26 22:27:38 +0300

    added graphql

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,13 @@
 'use strict';
+const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+  function adopt (value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled (value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+    function rejected (value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+    function step (result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
 const __importDefault = (this && this.__importDefault) || function (mod) {
   return (mod && mod.__esModule) ? mod : { default: mod };
 };
@@ -6,14 +15,20 @@ Object.defineProperty(exports, '__esModule', { value: true });
 const dotenv_1 = require('dotenv');
 const express_1 = __importDefault(require('express'));
 const client_1 = require('@prisma/client');
-(0, dotenv_1.config)();
-const app = (0, express_1.default)();
 const prisma = new client_1.PrismaClient();
 // use `prisma` in your application to read and write data in your DB
-console.log(prisma);
-app.get('/', (req, res, next) => {
-  res.send('Express server with TypeScript');
-});
+(0, dotenv_1.config)();
+const app = (0, express_1.default)();
+app.get('/', (req, res, next) => __awaiter(void 0, void 0, void 0, function * () {
+  const user = yield prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'asdsad@asad.as',
+      password: 'asdasd'
+    }
+  });
+  res.json(user);
+}));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,69 @@
 import { config } from 'dotenv';
 import express, { Application, NextFunction, Request, Response } from 'express';
+config();
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
-// use `prisma` in your application to read and write data in your DB
-config();
+
+
 
 const app: Application = express();
 
-app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+type User = {
+  id: string;
+  name: string | null;
+  lastName: string | null;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type User {
+    id: String!
+    name: String
+    lastName: String
+    email: String!
+    password: String!
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Query {
+    users: [User]
+    user(email: String): User
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  users: async (): Promise<User[]> => {
+    return await prisma.user.findMany();
+  },
+  user: async (args: {email: string}): Promise<User | null>=> {
+    return await prisma.user.findUnique({
+      where: {
+        email: args.email
+      }
+    })
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`
+  Server is listening on port http://localhost:${PORT} \n
+  graphiql running on http://localhost:${PORT}/graphql
+  `);
 });
