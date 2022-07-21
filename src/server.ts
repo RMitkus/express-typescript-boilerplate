6fa8d770edd30a2951f2c5c681f79d603bdd932d
Rytis Mitkus <77438479+RytisMitkus@users.noreply.github.com>
2022-07-21 22:06:03 +0300

    test

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,7 @@ const app: Application = express()
 
 app.use(morgan('dev'))
 app.use(express.json({
-	limit: '50mb',
-	extended: false
+	limit: '50mb'
 }))
 app.use(express.urlencoded({
 	limit: '50mb',
